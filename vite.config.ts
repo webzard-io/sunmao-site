12488ddfff8c880c86876ce7d26e372190cefdcf
Bowen Tan <bowen.tan@smartx.com>
2022-07-11 17:20:40 +0800

    don't use fsManager in production mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sunmaoFsVitePlugin from './tools/sunmao-fs-vite-plugin';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/installer/api': 'http://192.168.28.253',
+  build: {
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(
+          new URL('./index.html', import.meta.url)
+        ),
+        dev: fileURLToPath(
+          new URL('./dev.html', import.meta.url)
+        ),
+      },
     },
   },
+
   plugins: [
     sunmaoFsVitePlugin({
       schemas: [
